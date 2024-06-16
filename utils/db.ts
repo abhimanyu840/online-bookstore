e9fa9e594818eafb4b9f93a286deadc50c86422a
Abhimanyu Kumar <akabhimanyukumar111@gmail.com>
2024-06-16 22:38:21 +0530

    minor db tweak

--- utils/db.ts
+++ utils/db.ts
@@ -7,8 +7,8 @@ if (!MONGODB_URI) {
 }
 
 interface MongooseCache {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
+    conn: any;
+    promise: any;
 }
 
 declare global {
