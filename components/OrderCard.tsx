eb75c3e0693b64eadc31bc3257b1e5b4460bf962
Abhimanyu Kumar <akabhimanyukumar111@gmail.com>
2024-06-18 17:59:29 +0530

    minor design fix

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -10,15 +10,15 @@ interface OrderData {
 
 const OrderCard = (OrderData: OrderData) => {
     return (
-        <div className=' w-10/12 bg-green-200 dark:bg-gray-800 shadow-md dark:shadow-gray-800 border rounded-md my-4 mx-auto p-4 border-green-800 dark:border-blue-800'>
+        <div className=' w-10/12 bg-green-200 dark:bg-gray-800 shadow-md dark:shadow-gray-800 border rounded-md my-4 mx-auto p-2 md:p-4 border-green-800 dark:border-blue-800'>
             <div className='flex justify-between items-center p-4'>
                 <div className='flex items-center'>
                     <img src={OrderData.image} alt={OrderData.title} className='w-12 h-12 rounded' />
                     <div className='ml-4'>
-                        <h2 className='text-lg'>Book Name: {OrderData.title}</h2>
-                        <p className='text-sm'>Author: {OrderData.author}</p>
-                        <p className='text-sm'>Price: ${OrderData.price}</p>
-                        <p className='text-sm'>Quantity: {OrderData.quantity}</p>
+                        <h2 className='text-lg'><b><u>Book Name:</u></b> {OrderData.title}</h2>
+                        <p className='text-sm'><b><u>Author:</u></b> {OrderData.author}</p>
+                        <p className='text-sm'><b><u>Price:</u></b> ₹{OrderData.price}</p>
+                        <p className='text-sm'><b><u>Quantity:</u></b> {OrderData.quantity}</p>
                     </div>
                 </div>
             </div>
